38239d4cac705202940cec073777f95ab2b86250
srivatshan <srirenga96@gmail.com>
2020-02-03 00:39:56 +0530

    Completed the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
+import Quiz from './Quiz'
+let quizData = require('./quiz_data.json')
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <div >
+        <Quiz/>
+        </div>
     )
   }
 }
